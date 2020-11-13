684f603f7d77e75d1adfdfb404f1dfa30d5ea75d
cecarrionmontero <70933216+cecarrionmontero@users.noreply.github.com>
2020-11-13 15:38:00 -0500

    Update Install-asterisk16.deb10.sh

--- add-Install/asterisk/Install-asterisk16.deb10.sh
+++ add-Install/asterisk/Install-asterisk16.deb10.sh
@@ -9,6 +9,11 @@ sudo apt -y install wget locales snmp snmpd sngrep build-essential aptitude open
 #Install Asterisk 16 LTS dependencies
 sudo apt -y install git curl wget libnewt-dev libssl-dev libncurses5-dev subversion libsqlite3-dev build-essential libjansson-dev libxml2-dev  uuid-dev
 
+#Install MIBS
+sudo apt install smistrip -y
+sudo wget http://ftp.br.debian.org/debian/pool/non-free/s/snmp-mibs-downloader/snmp-mibs-downloader_1.2_all.deb
+sudo dpkg -i ./snmp-mibs-downloader_1.2_all.deb
+
 #Install freetds
 sudo apt -y install unixodbc unixodbc-dev freetds-dev freetds-bin tdsodbc
 
@@ -58,7 +63,6 @@ host = dbserver.domain.com
 port = 1433 
 tds version = 7.2 
 EOF
-cat "" >> /etc/freetds/freetds.conf
 cat /tmp/temp.txt >> /etc/freetds/freetds.conf
 
 #Add universe repository and install subversion
@@ -105,7 +109,7 @@ sudo systemctl restart asterisk
 sudo systemctl enable asterisk
 
 #Test to see if it connect to Asterisk CLI
-sudo asterisk -rvv
+#sudo asterisk -rvv
 
 #Mod /etc/asterisk/asterisk.conf
 sed -i 's/;highpriority = yes/highpriority = yes/g' /etc/asterisk/asterisk.conf
@@ -125,11 +129,25 @@ agentXSocket /var/agentx/master
 agentXPerms 0660 0550
 EOF
 
+sudo wget https://www.voztovoice.org/tmp/asterisk-mib.txt
+sudo wget https://www.voztovoice.org/tmp/digium-mib.txt
+mv asterisk-mib.txt /usr/share/snmp/mibs/ASTERISK-MIB.txt
+mv digium-mib.txt /usr/share/snmp/mibs/DIGIUM-MIB.txt
+
+echo "DIGIUM-MIB.txt" >> /usr/share/snmp/mibs/miblist.txt
+echo "ASTERISK-MIB.txt" >> /usr/share/snmp/mibs/miblist.txt
+
+sed -i 's/mibs :/#mibs :/g' /etc/snmp/snmp.conf
+
 systemctl enable snmpd
 systemctl start snmpd
 systemctl status snmpd
 
-snmpwalk -v2c -c MyServices 127.0.0.1 
+#snmpwalk -v2c -c MyServices 127.0.0.1 
+#snmpwalk -v2c -c MyServices 127.0.0.1 
+#snmpwalk -v2c -c MyServices 127.0.0.1 1.3.6.1.4.1.22736.1
+#snmpwalk -v2c -c MyServices 127.0.0.1 1.3.6.1.4.1.22736.1
+#nmpwalk -v2c -c MyServices 127.0.0.1 ASTERISK-MIB::astChanTypeName
 
 #open ports in ufw firewall
 sudo apt install -y ufw 
@@ -142,3 +160,39 @@ sudo ufw allow proto tcp from any to any port 5060,5061
 sudo ufw allow 10000:65000/udp
 sudo ufw allow 161
 sudo ufw enable
+
+#
+cat >> /tmp/temp.txt << EOF
+noload => cdr_manager.so 
+noload => cdr_pgsql.so
+noload => cdr_radius.so 
+noload => cdr_sqlite3_custom.so
+noload => pbx_lua.so 
+noload => pbx_dundi.so
+noload => res_parking.so
+noload => res_calendar.so
+noload => res_calendar_caldav.so
+noload => res_calendar_ews.so
+noload => res_calendar_exchange.so
+noload => res_calendar_icalendar.so
+noload => res_config_ldap.so
+noload => chan_pjsip.so
+noload => func_pjsip_aor.so
+noload => func_pjsip_contact.so
+noload => func_pjsip_endpoint.so
+noload => res_pjsip.so
+noload => res_pjsip_acl.so
+noload => res_pjsip_authenticator_digest.so
+noload => res_pjsip_caller_id.so
+noload => res_pjsip_config_wizard.so
+noload => res_pjsip_dialog_info_body_generator.so
+noload => res_pjsip_diversion.so
+noload => res_pjsip_dlg_options.so
+noload => res_pjsip_dtmf_info.so
+noload => res_pjsip_empty_info.so
+noload => res_pjsip_endpoint_identifier_anonymous.so
+noload => res_pjsip_endpoint_identifier_ip.so
+EOF
+cat /tmp/temp.txt >> /etc/asterisk/modules.conf
+
+sudo systemctl reboot
